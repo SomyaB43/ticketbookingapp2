6a5c93d93b446fc6bc43252ead3de546adf88e3e
Jumaana-bit <71558928+Jumaana-bit@users.noreply.github.com>
2024-03-30 14:46:15 -0400

    Create Utilities.java

---
+++ src/main/java/sofe3980/Utilities.java
@@ -0,0 +1,28 @@
+package sofe3980;
+
+import java.time.LocalTime;
+
+public class Utilities {
+
+    /**
+     * Converts time from 24-hour format to 12-hour format.
+     * 
+     * @param time The time in 24-hour format.
+     * @return The time in 12-hour format.
+     */
+    public static String convertTo12HourFormat(LocalTime time) {
+        // Implementation
+        return null;
+    }
+
+    /**
+     * Converts time from 12-hour format to 24-hour format.
+     * 
+     * @param time The time in 12-hour format.
+     * @return The time in 24-hour format.
+     */
+    public static LocalTime convertTo24HourFormat(String time) {
+        // Implementation
+        return null;
+    }
+}
