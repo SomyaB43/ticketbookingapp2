21d630abe9be5e2bb1c80682b20d8ad9e62cfaab
SomyaB43 <119441946+SomyaB43@users.noreply.github.com>
2024-03-31 00:36:38 -0400

    Update UserManager.java

--- src/main/java/sofe3980/UserManager.java
+++ src/main/java/sofe3980/UserManager.java
@@ -1,39 +1,31 @@
-package sofe3980;
-
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UserManager {
 
-    // If we are using a database, this will change to use that instead for storing
-    // users
     private List<User> users;
+    private int nextUserId; // To keep track of the next available user ID
 
     public UserManager() {
         this.users = new ArrayList<>();
+        this.nextUserId = 1; // Start with user ID 1
     }
 
-    /**
-     * Registers a new user with the given details and adds the user to the list of
-     * registered users.
-     * 
-     * @param name  The name of the user.
-     * @param email The email address of the user.
-     * @return The newly registered User object.
-     */
-    public User registerUser(String name, String email) {
-        return null;
+    public User registerUser(String name, String email, String password, LocalDate dob, String passportNumber) {
+        int userId = nextUserId++; // Assign the current value of nextUserId and then increment it
+        User newUser = new User(userId, name, email, password, dob, passportNumber);
+        users.add(newUser);
+        return newUser;
     }
 
-    /**
-     * Attempts to log in a user with the given email address.
-     * 
-     * @param email The email address of the user attempting to log in.
-     * @return An Optional containing the User object if login is successful, or an
-     *         empty Optional otherwise.
-     */
     public Optional<User> loginUser(String email, String password) {
-        return null;
+        for (User user : users) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
     }
 }
